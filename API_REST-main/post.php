4b952458982b5d191b6c5ba804b85055e9d9f328
jorgevargas <jorgerubenvargas84@gmail.com>
2024-05-18 19:58:12 -0600

    primera version

---
+++ API_REST-main/post.php
@@ -0,0 +1,89 @@
+<?php
+include "config.php";
+include "utils.php";
+
+
+$dbConn =  connect($db);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+
+      $sql = $dbConn->prepare("SELECT * FROM posts where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+
+      $sql = $dbConn->prepare("SELECT * FROM posts");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO posts
+          (title, status, content, user_id)
+          VALUES
+          (:title, :status, :content, :user_id)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    $postId = $dbConn->lastInsertId();
+    if($postId)
+    {
+      $input['id'] = $postId;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+	 }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$id = $_GET['id'];
+  $statement = $dbConn->prepare("DELETE FROM posts where id=:id");
+  $statement->bindValue(':id', $id);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postId = $input['id'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE posts
+          SET $fields
+          WHERE id='$postId'
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+
+
+header("HTTP/1.1 400 Bad Request");
+
+?>
